9231f721405294fe2850f62077bd9b71d1d16cf5
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2024-01-23 20:02:52 +0100

    insert cdn and script js

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-function slideHome (id) {
-    let items = document.querySelectorAll(id+'.carousel .carousel-item')
-    
-    items.forEach((el) => {
-        const minPerSlide = 1
-        let next = el.nextElementSibling
-        for (var i=1; i<minPerSlide; i++) {
-            if (!next) {
-                next = items[0]
-              }
-            let cloneChild = next.cloneNode(true)
-            el.appendChild(cloneChild.children[0])
-            next = next.nextElementSibling
-        }
-    })
-}
+// function slideHome(id) {
+//     let items = document.querySelectorAll(id + '.carousel .carousel-item')
 
-let carousels = document.querySelectorAll('.carousel.slide');
-for (const carousel of carousels) {
-    let id = '#' +carousel.id
-    slideHome(id);
-}
+//     items.forEach((el) => {
+//         const minPerSlide = 1
+//         let next = el.nextElementSibling
+//         for (var i = 1; i < minPerSlide; i++) {
+//             if (!next) {
+//                 next = items[0]
+//             }
+//             let cloneChild = next.cloneNode(true)
+//             el.appendChild(cloneChild.children[0])
+//             next = next.nextElementSibling
+//         }
+//     })
+// }
+
+// let carousels = document.querySelectorAll('.carousel.slide');
+// for (const carousel of carousels) {
+//     let id = '#' + carousel.id
+//     slideHome(id);
+// }
+
+let items = document.querySelectorAll('#iconCarousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 12;
+    let next = el.nextElementSibling
+    for (var i = 0; i < minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items[0];
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
