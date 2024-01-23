44c48bc1cb4b1f3d3648fb76b082cfaa4d80ce5a
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2024-01-23 19:41:25 +0100

    insert index.html and script.js in test

---
+++ script.js
@@ -0,0 +1,22 @@
+function slideHome (id) {
+    let items = document.querySelectorAll(id+'.carousel .carousel-item')
+    
+    items.forEach((el) => {
+        const minPerSlide = 1
+        let next = el.nextElementSibling
+        for (var i=1; i<minPerSlide; i++) {
+            if (!next) {
+                next = items[0]
+              }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    })
+}
+
+let carousels = document.querySelectorAll('.carousel.slide');
+for (const carousel of carousels) {
+    let id = '#' +carousel.id
+    slideHome(id);
+}
