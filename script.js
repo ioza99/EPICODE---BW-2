c8d9e6c7a5fc6a4c72d04ae50dc45d2d85b91075
DeadCleaver <andrealuca.montefiori@gmail.com>
2024-01-24 20:37:11 +0100

    commit 4
    aggiustato carousel di stanze, aggiunto card di prenotazioen

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 /* ricordarsi di cambiare id per questo carosello e non scombinare quelli delle altre pagine */
-let items = document.querySelectorAll('.carousel .carousel-item')
+let rooms = document.querySelectorAll('#roomsCarousel.carousel .carousel-item')
 
-		items.forEach((el) => {
+		rooms.forEach((el) => {
 			const minPerSlide = 3
 			let next = el.nextElementSibling
 			for (var i=1; i<minPerSlide; i++) {
 				if (!next) {
             // wrap carousel by using first child
-            next = items[0]
+            next = rooms[0]
         }
         let cloneChild = next.cloneNode(true)
         el.appendChild(cloneChild.children[0])
